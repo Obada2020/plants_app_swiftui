baacba9bd773e2d160d107e7c2e27c214ab5a219
Obada Alhalabi <abadah.alhalabi2020@gmail.com>
2024-06-24 11:21:00 +0400

    Initial Commit

---
+++ plants_app_swift/plants_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  plants_app_swiftApp.swift
+//  plants_app_swift
+//
+//  Created by Obada Alhalabi on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct plants_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
